9df0181631fdbbd137a69b9c2f86484aafe3cde8
ËshanDias <eash_di@ymail.com>
2018-08-11 16:34:46 +0530

    Ïnitital Commit with the layouts

---
+++ Unit Converter/DistanceViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DistanceViewController.swift
+//  Unit Converter
+//
+//  Created by Eshan Dias on 8/11/18.
+//  Copyright © 2018 Eshan Dias. All rights reserved.
+//
+
+import UIKit
+
+class DistanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
